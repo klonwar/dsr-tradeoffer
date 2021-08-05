14085c0ce36d7a5df9d711cac833334642492d3e
Klonwar <dima.arapov@gmail.com>
2021-08-05 13:29:54 +0300

    Логин и почта проверяются на уникальность сразу на первой странице

--- frontend/src/components/registration/dto/first-registration-step.dto.ts
+++ frontend/src/components/registration/dto/first-registration-step.dto.ts
@@ -1,10 +1,20 @@
 import { CreateUserDto } from '#server/common/dto/create-user.dto';
 import { Match } from '#server/common/validators/validator-extend-match-decorator';
-import { IsOptional } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsUsernameAvailable } from '#src/js/validators/validator-extend-is-username-available';
+import { IsEmailAvailable } from '#src/js/validators/validator-extend-is-email-available';
 
 export class FirstStepData extends CreateUserDto {
   @Match(FirstStepData, (s) => s.password, { message: `Пароли не совпадают` })
-  passwordConfirmation;
+  passwordConfirmation: string;
+
+  @IsUsernameAvailable({ message: `Это имя уже занято` })
+  @IsNotEmpty({ message: `Нужно указать логин` })
+  username: string;
+
+  @IsEmailAvailable({message: `Эта почта уже занята`})
+  @IsEmail(undefined, { message: `Некорректный email` })
+  email: string;
 
   @IsOptional()
   firstName;
