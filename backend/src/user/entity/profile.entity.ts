23d972221ee99d71082ad69ad9ef34f9d2a0b4f6
Klonwar <dima.arapov@gmail.com>
2021-08-06 13:33:03 +0300

    Сущности Profile и User теперь можно сериализовать
    + В User появились методы .toDto, .toJwtDto

--- backend/src/user/entity/profile.entity.ts
+++ backend/src/user/entity/profile.entity.ts
@@ -1,8 +1,10 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 import { User } from '#src/user/entity/user.entity';
+import { Exclude, Expose } from 'class-transformer';
 
 @Entity()
 export class Profile {
+  @Exclude()
   @PrimaryColumn()
   user_id: number;
 
@@ -18,10 +20,12 @@ export class Profile {
   @Column({ type: `date` })
   birthday: Date;
 
+  @Expose({ name: `photoPath` })
   @Column({ type: `text`, nullable: true })
   photo: string;
 
   // Relations
+  @Exclude()
   @OneToOne(() => User, { primary: true, onDelete: `CASCADE` })
   @JoinColumn({ name: `user_id` })
   user: User;
