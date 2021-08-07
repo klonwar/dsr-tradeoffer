b49afbf45c50794410906814fdc95d45eeaa9d8a
Klonwar <dima.arapov@gmail.com>
2021-08-07 20:55:02 +0300

    Исправление досадной ошибки в миграциях
    Надо было сразу делать чистым SQL, а не query builder-ом. При некоторых изменениях сущностей эти миграции просто не хотели запускаться из-за несоответствия типов. Сейчас все нормально

--- backend/src/db/migrations/1627571530665-FillUserTableWithAdmins.ts
+++ backend/src/db/migrations/1627571530665-FillUserTableWithAdmins.ts
@@ -1,42 +1,20 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { User, UserRole } from '../../user/entity/user.entity';
+import { UserRole } from '../../user/entity/user.entity';
 import * as bcrypt from 'bcrypt';
 
 export class FillUserTableWithAdmins1627571530665 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner
-      .manager
-      .createQueryBuilder()
-      .insert()
-      .into(`user`)
-      .values({
-        login: `admin`,
-        password: await bcrypt.hash(`admin_password`, 10),
-        role: UserRole.ADMIN,
-      } as User).execute();
+    await queryRunner.query(`INSERT INTO user (login, password, role)
+                             VALUES (?, ?, ?)`, [`admin`, await bcrypt.hash(`admin_password`, 10), UserRole.ADMIN]);
 
-    queryRunner
-      .manager
-      .createQueryBuilder()
-      .insert()
-      .into(`user`)
-      .values({
-        login: `moderator`,
-        password: await bcrypt.hash(`moderator_password`, 10),
-        role: UserRole.ADMIN,
-      } as User).execute();
+    await queryRunner.query(`INSERT INTO user (login, password, role)
+                             VALUES (?, ?, ?)`, [`moderator`, await bcrypt.hash(`admin_password`, 10), UserRole.ADMIN]);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner
-      .manager
-      .createQueryBuilder()
-      .delete().from(`user`)
-      .where(`login = :login1`)
-      .orWhere(`login = :login2`)
-      .setParameters({ login1: `admin`, login2: `moderator` })
-      .execute();
+    await queryRunner.query(`DELETE FROM user 
+                             WHERE login=? or login=?`, [`moderator`, `admin`]);
   }
 
 }
--- backend/src/db/migrations/1627804575519-FillProfileTableWithAdmins.ts
+++ backend/src/db/migrations/1627804575519-FillProfileTableWithAdmins.ts
@@ -1,67 +1,50 @@
-import { Profile } from '../../user/entity/profile.entity';
-import { User, UserRole } from '../../user/entity/user.entity';
+import { UserRole } from '../../user/entity/user.entity';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 interface UserTextRow {
-    user_id: number,
-    user_login: string,
-    user_password: string,
-    user_role: UserRole
+  id: number,
+  login: string,
+  password: string,
+  role: UserRole
 }
 
 const getAdmins = async (queryRunner: QueryRunner): Promise<UserTextRow[]> => {
-    return await queryRunner
-      .manager
-      .createQueryBuilder()
-      .select(`user`)
-      .from(User, `user`)
-      .where(`role = :role`, { role: UserRole.ADMIN })
-      .execute();
+  return await queryRunner.query(`SELECT *
+                                  FROM user
+                                  WHERE role = ?`, [UserRole.ADMIN]);
 };
 
 export class FillProfileTableWithAdmins1627804575519 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        const admins = await getAdmins(queryRunner);
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const admins = await getAdmins(queryRunner);
 
-        await queryRunner
-          .manager
-          .createQueryBuilder()
-          .insert()
-          .into(Profile)
-          .values({
-              user_id: admins[0][`user_id`],
-              email: `admin1@tradeoffer.ru`,
-              firstName: `Admin 1`,
-              phone: `8005553535`,
-              birthday: new Date(`01.01.1990`),
-          }).execute();
+    await queryRunner.query(`INSERT INTO profile (user_id, email, firstName, phone, birthday)
+                             VALUES (?, ?, ?, ?, ?)`, [
+      admins[0][`id`],
+      `admin1@tradeoffer.ru`,
+      `Admin 1`,
+      `8005553535`,
+      `1990-01-01`,
+    ]);
 
-        await queryRunner
-          .manager
-          .createQueryBuilder()
-          .insert()
-          .into(Profile)
-          .values({
-              user_id: admins[1][`user_id`],
-              email: `admin2@tradeoffer.ru`,
-              firstName: `Admin 2`,
-              phone: `8005553535`,
-              birthday: new Date(`01.01.1990`),
-          }).execute();
-    }
+    await queryRunner.query(`INSERT INTO profile (user_id, email, firstName, phone, birthday)
+                             VALUES (?, ?, ?, ?, ?)`, [
+      admins[1][`id`],
+      `admin2@tradeoffer.ru`,
+      `Admin 2`,
+      `8005553535`,
+      `1990-01-01`,
+    ]);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        const admins = await getAdmins(queryRunner);
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const admins = await getAdmins(queryRunner);
 
-        await queryRunner
-          .manager
-          .createQueryBuilder()
-          .delete().from(Profile)
-          .where(`user_id = :id1`)
-          .orWhere(`user_id = :id2`)
-          .setParameters({ id1: admins[0][`user_id`], id2: admins[1][`user_id`] })
-          .execute();
-    }
+    await queryRunner.query(`DELETE
+                             FROM profile
+                             WHERE user_id = ?
+                                OR user_id = ?`, [admins[0][`id`], admins[1][`id`]]);
+  }
 
 }
