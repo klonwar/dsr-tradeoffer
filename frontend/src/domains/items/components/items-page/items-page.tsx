e00d6b41354854635a1ebd9b8732a6d3c0d07bb8
Klonwar <dima.arapov@gmail.com>
2021-08-13 08:49:28 +0300

    Небольшие изменения стилей

--- frontend/src/domains/items/components/items-page/items-page.tsx
+++ frontend/src/domains/items/components/items-page/items-page.tsx
@@ -5,8 +5,8 @@ import { ItemsList } from '../items-list/items-list';
 export const ItemsPage: FC = () => {
   return (
     <div className={`ItemsContainer`}>
-      <div className={`ItemsContainer-content MainWrapper-content--noPadding`}>
-        <div className={`ItemsContainer-actions uk-flex uk-width-1-1 uk-flex-center uk-padding-small`}>
+      <div className={`ItemsContainer-content`}>
+        <div className={`ItemsContainer-actions`}>
           <Link to={`/items/add`} className={`uk-button uk-button-default uk-flex uk-flex-middle`}>
             <span className={`ItemsContainer-actionIcon`} uk-icon={`icon: plus`} />
             <span className={`uk-margin-small-left uk-visible@s`}>Вещь</span>
