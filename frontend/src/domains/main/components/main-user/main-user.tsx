f9c48376974bf49700321cb68180e827dcea6559
Klonwar <dima.arapov@gmail.com>
2021-08-13 18:35:12 +0300

    Добавление ссылок на главную страницу

--- frontend/src/domains/main/components/main-user/main-user.tsx
+++ frontend/src/domains/main/components/main-user/main-user.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { userDataSelector } from '#redux/selectors';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const MainUser: FC = () => {
   const userData = useSelector(userDataSelector);
@@ -12,8 +13,8 @@ export const MainUser: FC = () => {
           <h1>Здравствуйте, {userData.firstName}</h1>
           <h4>Вы успешно вошли в свой аккаунт и можете пользоваться сайтом! Вот, что вы можете сделать:</h4>
           <ul>
-            <li>Добавить новую вещь для обмена</li>
-            <li>Посмотреть на свои выложенные вещи</li>
+            <li>Добавить <Link to={`/items/add`}>новую вещь</Link> для обмена</li>
+            <li>Посмотреть на свои <Link to={`/items/`}>выложенные вещи</Link></li>
             <li>Обменяться с другим пользователем <span className={`uk-text-muted`}>(Пока в разработке)</span></li>
           </ul>
         </div>
