863a42db8fb8c1983b33f9e0d2f779024a05fe3e
Klonwar <dima.arapov@gmail.com>
2021-08-16 14:52:12 +0300

    Заглушка на странице показа вещи при отсутствии фотографии

--- frontend/src/domains/items/components/photos-slideshow/photos-slideshow.tsx
+++ frontend/src/domains/items/components/photos-slideshow/photos-slideshow.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { ItemDto } from '#server/common/dto/item.dto';
 import { srcFromPhotoPath } from '#src/js/util/src-from-photo-path';
+import noPhoto from '#src/icons/no-photo.svg';
 
 interface Props {
   item: ItemDto;
@@ -9,16 +10,19 @@ interface Props {
 }
 
 export const PhotosSlideshow: FC<Props> = ({ item, width = `xlarge`, padding = true }) => {
-  if (!(item?.photos && item.photos.length !== 0)) {
-    return null;
-  }
+  const isNoPhoto = !(item?.photos && item.photos.length !== 0);
 
   return (
     <div className={`ItemPhotos ${(padding) ? `ItemPhotos--padding@m` : ``} uk-width-${width} uk-position-relative uk-margin-bottom`} uk-slideshow={``}>
       <ul className={`uk-slideshow-items`}>
-        {item.photos?.map(({ photoPath }) => (
-          <li key={photoPath}><img src={srcFromPhotoPath(photoPath)} alt={``} uk-cover={``} /></li>
-        ))}
+        {(isNoPhoto) ? (
+          <li><img src={noPhoto} alt={``} uk-cover={``} /></li>
+        ) : (
+          item.photos?.map(({ photoPath }) => (
+            <li key={photoPath}><img src={srcFromPhotoPath(photoPath)} alt={``} uk-cover={``} /></li>
+          ))
+        )}
+        {}
       </ul>
       <button
         className={`uk-position-center-left uk-position-small uk-hidden-hover uk-link`}
