1c200f700cf3b9945c3beee94f92d9dfb90b1b82
Klonwar <dima.arapov@gmail.com>
2021-08-24 15:06:29 +0300

    В корзине одиночное радио будет активировано автоматически

--- frontend/src/domains/basket/components/basket-content-radios/basket-content-radios.tsx
+++ frontend/src/domains/basket/components/basket-content-radios/basket-content-radios.tsx
@@ -14,14 +14,16 @@ interface Props {
 
 export const BasketContentRadios: FC<Props> = (props) => {
   const { basketContent, name, useForm: { register, errors } } = props;
+  const basketValues = Object.values(basketContent);
+
   return (
     <div className={`uk-flex uk-flex-column uk-width-1-1 uk-width-2-3@s uk-height-1-1`}>
       <h3 className={`uk-text-muted uk-text-center uk-margin-top`}>{keyToLabelText.get(name)}</h3>
       <div className={`WithScrollbar uk-overflow-auto`}>
-        {Object.values(basketContent).map((item) => (
+        {basketValues.map((item) => (
           // label, потому что так чекбокс будет кликабельным
           <label key={item.id} className={`uk-flex uk-flex-middle`}>
-            <input {...register(name)} value={item.id} type={`radio`} className={`uk-radio`} />
+            <input {...register(name)} defaultChecked={basketValues.length === 1} value={item.id} type={`radio`} className={`uk-radio`} />
             <div className={`uk-padding-small uk-width-expand`}>
               <ItemCard item={item} isUserItem={name === `offered_item_id`} withItemActions={false} />
             </div>
